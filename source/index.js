53e95265b991f6d916ba46d270f87b3ebb57d696
Angelisium <angelisium@daum.net>
2023-04-19 12:07:57 +0200

    Create index.js

---
+++ source/index.js
@@ -0,0 +1,52 @@
+const jsxLite = (function () {
+	const TextNode = "__TEXT_NODE__";
+
+	function addChild(target, el) {
+		if (el instanceof Text || el instanceof HTMLElement) {
+			target.appendChild(el);
+		} else if (typeof el === "string") {
+			target.appendChild(createElement(TextNode, {}, el));
+		} else {
+			throw "Unexpected child type: " + typeof el;
+		}
+	}
+
+	function createElement(type, attribute = {}, children = []) {
+		/**********************************************************************
+		 * Create Element                                                     *
+		 **********************************************************************/
+		if (type === TextNode) {
+			if (typeof children != "string") {
+				throw "A text node cannot contain anything other than text.";
+			}
+			return document.createTextNode(children);
+		}
+		let element = document.createElement(type);
+		/**********************************************************************
+		 * Add Attribute                                                      *
+		 **********************************************************************/
+		for (let [attr, value] of Object.entries(attribute)) {
+			element.setAttribute(attr, value);
+		}
+		/**********************************************************************
+		 * Add Children                                                       *
+		 **********************************************************************/
+		if (children instanceof Array) {
+			for (let el of children) {
+				addChild(element, el);
+			}
+		} else {
+			addChild(element, children);
+		}
+
+		return element;
+	}
+
+	function createElementBabel(type, options, ...children) {
+		return createElement(type, options, children);
+	}
+
+	return { createElement, createElementBabel };
+})();
+
+module.exports = jsxLite;
