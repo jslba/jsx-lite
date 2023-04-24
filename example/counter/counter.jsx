8d017ea2fa382bcea5c8d3c446f689c8ae9c47a9
Angelisium <angelisium@daum.net>
2023-04-24 18:29:35 +0200

    Update counter.jsx

--- example/counter/counter.jsx
+++ example/counter/counter.jsx
@@ -1,17 +1,15 @@
-var count = 0;
-
-function formatCount(count) {
-	return "Count is " + count + "!";
-}
-
-function counter() {
-	let out = document.querySelector("#output");
-	out.textContent = formatCount(++count);
-}
-
 const root = document.querySelector("#root");
+
+var count = 0;
+count.__proto__.toString = () => "Count is " + count + "!";
+const output = <p>{count.toString()}</p>;
+count.__proto__.next = function () {
+	count++;
+	output.textContent = count.toString();
+};
+
 root.append(
 	<h1>Counter</h1>,
-	<p id="output">{formatCount(count)}</p>,
-	<button onclick={counter}>Up</button>
+	output,
+	<button onclick={count.next}>Up</button>
 );
